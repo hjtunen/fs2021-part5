75f93814db8d94104a7e04e31be2340d0172de36
hjtunen <91310858+hjtunen@users.noreply.github.com>
2022-01-04 12:42:59 +0200

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,45 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        <div> 
+          {blog.title} {blog.author}
+          <button onClick={toggleVisibility}>hide</button>
+          <br></br>
+          {blog.url}
+          <br></br>
+          likes {blog.likes} 
+          <button>like</button>
+          <br></br>
+          {blog.user.name}
+        </div>
+    </div>
+  )}
+
+  return (
+    <div style={blogStyle}>
+      <div> 
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+  </div>
+)}
+
 
 export default Blog
